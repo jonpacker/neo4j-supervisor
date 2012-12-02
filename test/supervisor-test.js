09d90ffe9536ee2b5d51a2bd97b2dc440255ec28
Jon Packer <contact@jonpacker.com>
2012-12-02 20:36:59 +0100

    added test for starting a server, added better setup/teardown

--- test/supervisor-test.js
+++ test/supervisor-test.js
@@ -5,6 +5,7 @@ var nvm = require('neo4j-vm');
 
 describe('supervisor', function() {
   var serverpath;
+  var neo;
   before(function(done) {
     nvm('1.9.M01', 'community', false, function(err, path) {
       if (err) return done(err);
@@ -13,11 +14,28 @@ describe('supervisor', function() {
     });
   });
 
+  beforeEach(function() {
+    neo = sv(serverpath);
+  });
+
+  afterEach(function(done) {
+    neo.stop(done);
+  });
+
   it('should check if a server is running', function(done) {
-    var neo = sv(serverpath);
     sv.running(function(yep) {
       assert.ok(!yep);
       done();
+    });
+  });
+
+  it('should start a server', function(done) {
+    sv.start(function(err) {
+      assert.ok(!err);
+      sv.running(function(yep) {
+        assert.ok(yep);
+        done();
+      });
     });
   });
 });
