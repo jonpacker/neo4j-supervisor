df68c1da4947493a00a1fc9593060519f43d35c8
Jon Packer <contact@jonpacker.com>
2012-12-03 21:00:28 +0100

    added start/stop

--- neosv.js
+++ neosv.js
@@ -1,9 +1,49 @@
-function supervisor(serverpath) {
+var spawn = require('child_process').spawn;
+var naan = require('naan');
+var join = require('path').join;
+var fs = require('fs');
+
+//Run the given neo4j instance with the given command and return the output
+
+var supervisor = function (serverpath) {
   if (!(this instanceof supervisor)) {
     return new supervisor(serverpath);
   }
   
-  this.server = serverpath;
+  this.server = {
+    path: serverpath,
+    bin: join(serverpath, 'bin/neo4j')
+  };
 }
+
+supervisor.prototype._run = function(command, callback) {
+  var neo = spawn(this.server.bin, [command]);
+  var output = '';
+  var error = '';
+  neo.stdout.on('data', function(data) {
+    output += data;
+  });
+  neo.stderr.on('data', function(data) {
+    error += data;
+  });
+  neo.on('exit', function(code) {
+    if (code) callback(new Error(error || output));
+    else callback(null, output);
+  });
+};
+
+supervisor.prototype.running = function(callback) {
+  this._run('status', function(err, status) {
+    if (err) return callback(err);
+    callback(null, !!/pid\s+\d+/.exec(status));
+  });
+};
+
+supervisor.prototype.start = function(callback) {
+  this._run('start', callback);
+};
+supervisor.prototype.stop = function(callback) {
+  this._run('stop', callback);
+};
 
 module.exports = supervisor;
